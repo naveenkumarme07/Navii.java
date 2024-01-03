2eb8f222b3fe2008c0db7743af6831e4c76a6ae9
naveenkumarme07 <[200~>
2024-01-03 11:12:31 +0530

    Maths

---
+++ MathMethods.java
@@ -0,0 +1,25 @@
+
+
+public class MathMethods {
+	
+	
+
+	public static void main(String[] args) {
+		
+                int math=Math.abs(-5);
+                System.out.println(math);
+                
+               int valvue= Math.addExact(9 , 8);
+               System.out.println(valvue);
+               
+               double coshh=Math.cosh(12.4);
+               System.out.println(coshh);
+                
+               double num1=Math.max(45.2, 98.3);
+               System.out.println(num1);
+               
+               int num2=Math.multiplyExact(89,65);
+               System.out.println(num2);
+	}
+
+}
