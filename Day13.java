fc8a4c0addac3685237bbfebb29a35cae6221976
naveenkumarme07 <[200~>
2024-01-04 10:08:48 +0530

    stringss

---
+++ Day13.java
@@ -0,0 +1,66 @@
+package Patterns;
+
+public class Day13 {
+
+	public static void main(String[] args) {
+                
+		String nationanAnimal="lion";
+		
+		System.out.println(nationanAnimal);
+		
+		nationanAnimal="tiger";
+		
+		 String karnatakaCapital="banglore";
+		  
+		 String siliconCity="banglore";
+		 
+		 System.out.println(siliconCity==karnatakaCapital);
+		 
+		
+		 String cricketGod="Sachin";
+		 
+		 cricketGod="kohli";
+		 System.out.println(cricketGod);
+		 
+		 String boringClass="mathmatics";
+		 
+		 String favoriteClass="mathmatics";
+		 System.out.println(boringClass==favoriteClass);
+		 
+		 String powerfullGod="anajaneya";
+		 
+		 powerfullGod="Rama";
+		 String course="java";
+		 
+		//////     new key word
+		 
+		 
+		                        String fvrtIpl=new String("RCB");
+		                        System.out.println(fvrtIpl);
+		                        fvrtIpl="CSK";
+		                        
+		                        System.out.println(fvrtIpl==fvrtIpl);
+		                        
+		                        String SpecialLove=new String("amma");
+		                                     
+		                        String favouritePerson=new String("amma");
+		                        System.out.println(SpecialLove==favouritePerson);
+		                         
+		                         String favouritePlace=new String("mynative");
+		                         favouritePlace="banglore";
+		                         
+		                         String fathertOfPakistan=new String("kohli");
+		                         
+		                         String favouriteBook=new String("richDad-poorDad");
+		                         favouriteBook="parisaraPremi";
+		                         
+		                         String toughestCourse=new String ("java");
+		                         
+		                         System.out.println(toughestCourse==course);
+		                         
+		                         
+	}
+	
+	
+
+}
