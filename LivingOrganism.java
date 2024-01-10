f9dcc9cef8087f17d60a991b0d8572050e2f2808
naveenkumarme07 <[200~>
2024-01-10 17:01:01 +0530

    inheritanceclass

---
+++ LivingOrganism.java
@@ -0,0 +1,21 @@
+package Working;
+
+public class LivingOrganism {
+	
+	   String ScintificName;
+	    int LifeSpan;
+	    
+	    void Takingoxygen(){
+	    	
+	    }
+	    
+	    void Sleeping(){
+	    	
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
