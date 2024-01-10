ee14318289740c84e6b1f2fba5479bd5b80d1343
naveenkumarme07 <[200~>
2024-01-10 09:39:10 +0530

    bus is a vehivle

---
+++ Vehicle.java
@@ -0,0 +1,46 @@
+package Working;
+
+public class Vehicle {
+	
+	int numofseats;
+	String busnames;
+	int numofmirrors;
+	String ownerName;
+	
+	void Travelling(){
+		
+	}
+	 boolean canFly() {
+        return canFly();
+    }
+	void makeSound(){
+		
+	}
+	void Start(){
+		
+	}
+	
+	class bus extends Vehicle{
+		String name;
+		String colour;
+		
+		
+		void Stop(){
+			
+		}
+		void Openingdoor(){
+			
+		}
+	}
+ class car extends Vehicle{
+	 int numofmirrors;
+	 String Model;
+	 
+	 void closingFDoor(){
+		 
+	 }
+	 public void checkFuelLevel() {
+	        System.out.println("Fuel level: " );
+	    }
+ }
+}
