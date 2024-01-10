d0cc48993a38d843fa6235ebfeabfa5e577124ec
naveenkumarme07 <[200~>
2024-01-10 17:02:03 +0530

    wildAnimals

---
+++ Wild.java
@@ -0,0 +1,38 @@
+package Working;
+
+public class Wild extends Animals{
+      String Forestname;
+      int NumOfAnimals;
+      
+      void roarSound(){
+    	  
+      }
+      void eatingFood(){
+    	  
+      }
+      
+      class Lion extends Wild{
+    	  boolean isBeard;
+    	  String gender;
+    	  
+    	  void Hunting(){
+    		  
+    	  }
+    	  void climbingTree(){
+    		  
+    	  }
+      }
+      public class Elephant extends Wild {
+
+    	    boolean hasTrunk;  
+    	    boolean herbivorous; 
+    	    
+    	    void trumpet() {
+    	        System.out.println("The elephant is trumpeting loudly.");
+    	    }
+    	    void helping(){
+    	    	
+    	    }
+       }
+                          
+     }
